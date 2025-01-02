c450c95bb1ec766505c79c56e2118804d755e199
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-01-02 18:40:58 +0500

    added a few changes to navbar

--- src/components/temps/Navbar.jsx
+++ src/components/temps/Navbar.jsx
@@ -10,26 +10,26 @@ const Navbar = () => {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <div className="navbar-logo">
-          <h1>YourLogo</h1>
-        </div>
-        <div className={`navbar-menu ${isOpen ? 'open' : ''}`}>
-          <Link to="home" onClick={toggleMenu}>Home</Link>
-          <Link to="about" onClick={toggleMenu}>About</Link>
-          <Link to="projects" onClick={toggleMenu}>Projects</Link>
-          <Link to="skills" onClick={toggleMenu}>Skills</Link>
-          <Link to="contact" onClick={toggleMenu}>Contact</Link>
-        </div>
-        <div className="navbar-toggle" onClick={toggleMenu}>
-          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-        </div>
-      </div>
-    </nav>
-  );
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="navbar-logo">
+                    <h1> YourLogo </h1>
+                </div>
+                <div className={`navbar-menu ${isOpen ? 'open' : ''}`}>
+                    <Link to="home" onClick={toggleMenu}> Home </Link>
+                    <Link to="about" onClick={toggleMenu}> About </Link>
+                    <Link to="projects" onClick={toggleMenu}> Projects </Link>
+                    <Link to="skills" onClick={toggleMenu}> Skills </Link>
+                    <Link to="contact" onClick={toggleMenu}> Contact </Link>
+                </div>
+                <div className="navbar-toggle" onClick={toggleMenu}>
+                    <div className={`bar ${isOpen ? 'open' : ''}`}> </div>
+                    <div className={`bar ${isOpen ? 'open' : ''}`}> </div>
+                    <div className={`bar ${isOpen ? 'open' : ''}`}> </div>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
